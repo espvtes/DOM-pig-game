3b3371251ff455210965d02ca3fbf2be31fe36ae
yusniel Espino <espvtes@gmail.com>
2019-07-16 20:38:04 -0400

    hold the score and update

--- app.js
+++ app.js
@@ -67,9 +67,10 @@ document.querySelector('.btn-roll').addEventListener('click', function() {//the 
         document.getElementById('current-1').textContent = '0';
         document.querySelector('.player-0-panel').classList.toggle('active');
         document.querySelector('.player-1-panel').classList.toggle('active');
-
         // document.querySelector('.player-1-panel').classList.remove('active');
         // document.querySelector('.player-1-panel').classList.add('active');
+
+        document.querySelector('.dice').style.display = 'none';
         
     }
 

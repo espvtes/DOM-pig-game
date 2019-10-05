0e97693fa66882de62523c5f3df00483b70fbb98
Yusniel Espino <espvtes@gmail.com>
2019-10-04 20:40:38 -0400

    change the player

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     }else{
         //Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     }
 })
 

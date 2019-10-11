eca3336b3c459aef341b01a4380aee56038e08a1
Yusniel Espino <espvtes@gmail.com>
2019-10-10 22:24:18 -0400

    set the max point

--- app.js
+++ app.js
@@ -177,7 +177,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     
     
         //check if the player won
-        if(scores[activePlayer] >= 20){
+        if(scores[activePlayer] >= 100){
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!!!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
